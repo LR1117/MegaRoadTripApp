d707771ebd6eacc58a71ee18d270ea0c356c9340
Liam R <Liamri28@sssas.org>
2026-06-26 17:41:57 -0400

    Initial Commit

---
+++ MegaRoadTripApp/MegaRoadTripAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaRoadTripAppApp.swift
+//  MegaRoadTripApp
+//
+//  Created by Liam Riedy on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MegaRoadTripAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
